a9a21c26177f482a39e82073f99f0ffa23f3bece
tiancai-1228 <sky0975657713@gmail.com>
2023-03-10 16:36:48 +0800

    fix error

--- src/navigator/stack/auth.stack.tsx
+++ src/navigator/stack/auth.stack.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoginScreen from '@/screens/ogin/LoginScreen';
+import LoginScreen from '@/screens/login/LoginScreen';
 import SplashScreen from '@/screens/SplashScreen';
 import { RootStack } from '../rootNavigator';
 
