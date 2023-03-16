6bc0996ebddb4ab4ef254f7f8696694684a880ee
tiancai-1228 <sky0975657713@gmail.com>
2023-03-16 13:42:39 +0800

    remove StyleSheet

--- src/screens/report/component/view/Record.View.tsx
+++ src/screens/report/component/view/Record.View.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { record, recordType } from '@/type/bookkeeping';
 
 interface Prop {
